aa8e73304e38b12b1403ac49f7e31d2ef19a40f4
etaylor5 <etaylor5@terpmail.umd.edu>
2013-12-13 20:56:05 -0500

    Create Driver.java

---
+++ CMSC131-Project4/Driver.java
@@ -0,0 +1,15 @@
+package GUI;
+import framework.Controller;
+
+public class Driver {
+  private final static String RED_TEAM = "Easy";
+  private final static String BLUE_TEAM = "Medium";
+  private final static int ROWS = 14;
+  private final static int COLS = 28;
+  private final static int TEAM_SIZE = 50;
+  private final static int OBSTACLES = 30;
+  
+  public static void main(String[] args) {
+    new Controller(RED_TEAM, BLUE_TEAM, ROWS, COLS, TEAM_SIZE, OBSTACLES);
+  }
+}
