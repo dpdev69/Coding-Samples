df782bb337cb89e87eb880905c25ac1e79225bfd
etaylor5 <etaylor5@terpmail.umd.edu>
2013-12-14 05:34:39 -0500

    Create IllegalPondSizeException.java

---
+++ CMSC131-Project8/IllegalPondSizeException.java
@@ -0,0 +1,19 @@
+package fishPond;
+
+/**
+ * Thrown when someone tries to construct a Pond that is just too small!
+ *
+ * @author Fawzi Emad
+ */
+public class IllegalPondSizeException extends RuntimeException {
+  /* Field required by all Serializable classes */
+  private static final long serialVersionUID = 0;
+  
+  /**
+   * @param rows number of rows attempted
+   * @param cols number of columns attempted
+   */
+  public IllegalPondSizeException(int rows, int cols) {
+    super("Ilegal Pond size: " + rows + " rows by " + cols + " cols");
+  }
+}
