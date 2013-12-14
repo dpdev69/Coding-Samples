ec5a2ba4f2534fc6a0f8400aee0eb8062ab2d263
etaylor5 <etaylor5@terpmail.umd.edu>
2013-12-14 06:53:39 -0500

    Create mux2g.v

---
+++ ENEE245-Lab2/mux2g.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    00:32:30 10/21/2013 
+// Design Name: 
+// Module Name:    mux2g 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module mux2g(
+    input a,
+    input b,
+    input s,
+    output z
+    );
+
+
+endmodule
