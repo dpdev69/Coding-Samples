00a83e9f64455d4dd563675f6f9f57804aab46be
etaylor5 <etaylor5@terpmail.umd.edu>
2013-12-13 19:44:39 -0500

    Create JavaHistory.java

---
+++ CMSC131-Project1/JavaHistory.java
@@ -0,0 +1,44 @@
+/* This is a very simple Java program that will print some stuff about
+ * the history of the Java programming language.  
+ * Try running the program to see it working.  
+ * 
+ * Obviously, the facts stated in the output are not correct
+ * because there are several possible words with slashes between!  
+ * FIX THEM by editing the source-code below - changing as little as 
+ * possible on each line, and then submit the project.
+ * For information that is incorrect (i.e. your name), completely
+ * erase the old one and insert the correct information in its place. 
+ * When taking out the incorrect options, delete them and the slashes
+ * that appear between the options so that the result reads as a 
+ * correct sentence.  Use
+ * http://www.oracle.com/technetwork/java/javase/overview/javahistory-index-198355.html 
+ * to determine which value is correct in each set.  
+ * Please don't change the overall meaning of each 
+ * item in the output or change the other words around it.
+ * 
+ * Please don't add or remove any lines from the output -- otherwise you will 
+ * confuse the automated testing setup and you are likely not to pass the tests!
+ * (This is not the time to be creative!)
+ * 
+*/
+
+public class JavaHistory {
+
+	public static void main(String[] args) {
+		
+		System.out.print("First Introduce Yourself: ");  
+	    System.out.println("Hello, my name is Emmanuel Taylor.");
+	    System.out.println("I am currently registered in CMSC131 section:0102.");
+	    System.out.println("My instructor's name is Jandelyn Plane.");
+	    System.out.println("My TA's first name is Uran.");
+	    System.out.println();
+	    System.out.println("Some history of Java I have learned:");
+		System.out.println("Started in the early 1990's");
+		System.out.println("as the Green project.");
+		System.out.println("This team was part of Sun.");
+	    System.out.println("In 1995, it was used in a browser.");
+	    System.out.println("That browser was Netscape.");
+
+	}
+
+}
