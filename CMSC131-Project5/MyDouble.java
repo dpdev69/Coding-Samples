e0a70580748879421d08733febdb282333f3707a
etaylor5 <etaylor5@terpmail.umd.edu>
2013-12-13 23:14:58 -0500

    Update MyDouble.java

--- CMSC131-Project5/MyDouble.java
+++ CMSC131-Project5/MyDouble.java
@@ -49,85 +49,85 @@ public class MyDouble {
   }
 
 /**
-	 * Returns the product of the current object and the parameter.
-	 * Note:  This method does not modify the current object.
-	 * @param passedVal the value that serves as the second operand for the multiplication
-	 * @return the product of the current object and the parameter
-	 */
-	public MyDouble multiply(MyDouble passedVal) {
-		return new MyDouble(this.doubleValue * passedVal.doubleValue);
-		
-	}
+ * Returns the product of the current object and the parameter.
+ * Note:  This method does not modify the current object.
+ * @param passedVal the value that serves as the second operand for the multiplication
+ * @return the product of the current object and the parameter
+ */
+ public MyDouble multiply(MyDouble passedVal) {
+   return new MyDouble(this.doubleValue * passedVal.doubleValue);
+ }
+ 
+ /**
+  * Returns the quotient obtained when dividing the current object by the parameter.
+  * Note:  This method does not modify the current object. 
+  * @param passedVal the value that serves as the divisor
+  * @return the result of dividing the current object by the parameter
+  */
+ public MyDouble divide(MyDouble passedVal) {
+   return new MyDouble(this.doubleValue / passedVal.doubleValue);
+ }
 	
-	/**
-	 * Returns the quotient obtained when dividing the current object by the parameter.
-	 * Note:  This method does not modify the current object. 
-	 * @param passedVal the value that serves as the divisor
-	 * @return the result of dividing the current object by the parameter
-	 */
-	public MyDouble divide(MyDouble passedVal) {
-		return new MyDouble(this.doubleValue / passedVal.doubleValue);
-	}
+ /**
+  * Returns a MyDouble representing the square root of the current object.
+  * Note:  This method does not modify the current object.
+  * @return the square root of the current object
+  */
+ public MyDouble sqrt() {
+   return new MyDouble(Math.sqrt(this.doubleValue));
+ }
 	
-	/**
-	 * Returns a MyDouble representing the square root of the current object.
-	 * Note:  This method does not modify the current object.
-	 * @return the square root of the current object
-	 */
-	public MyDouble sqrt() {
-		return new MyDouble(Math.sqrt(this.doubleValue));
-	}
-	
-	/**
-	 * Compares the current object to the parameter.
-	 * @param passedVal the object being compared with the current object
-	 * @return a negative value if the current object is less than the parameter,
-	 * zero if the current object equals the parameter, a positive value if the 
-	 * current object is larger than the parameter.  NOTE:  Due to the lack
-	 * of precision in comparing floating point values, in cases where the two
-	 * values are NEARLY equal, this method will return 0.
-	 */
-	public int compareTo(MyDouble passedVal) {
-		if (Math.abs(this.doubleValue - passedVal.doubleValue) < EPSILON)
-			return 0;
-		else if (this.doubleValue < passedVal.doubleValue)
-			return -1;
-		else
-			return 1;
-	}  
+ /**
+  * Compares the current object to the parameter.
+  * @param passedVal the object being compared with the current object
+  * @return a negative value if the current object is less than the parameter,
+  * zero if the current object equals the parameter, a positive value if the 
+  * current object is larger than the parameter.  NOTE:  Due to the lack
+  * of precision in comparing floating point values, in cases where the two
+  * values are NEARLY equal, this method will return 0.
+  */
+ public int compareTo(MyDouble passedVal) {
+   if (Math.abs(this.doubleValue - passedVal.doubleValue) < EPSILON)
+     return 0;
+   else if (this.doubleValue < passedVal.doubleValue)
+     return -1;
+   else
+     return 1;
+ }  
 
-/**
-	 * Checks if the current object is equal to the parameter.  
-	 * 
-	 * @param passedVal the object being compared for equality with the current object
-	 * @return true if the current object is equal to the parameter, false
-	 * otherwise.  NOTE:  Due to the lack
-	 * of precision in comparing floating point values, in cases where the two
-	 * values are NEARLY equal, this method will return true.
-	 */
-	public boolean equals(MyDouble passedVal) {
-		if (passedVal == null){
-			return false;
-		}
-		return Math.abs(this.doubleValue - passedVal.doubleValue) < EPSILON;
-	}
+ /**
+  * Checks if the current object is equal to the parameter.  
+  * 
+  * @param passedVal the object being compared for equality with the current object
+  * @return true if the current object is equal to the parameter, false
+  * otherwise.  NOTE:  Due to the lack
+  * of precision in comparing floating point values, in cases where the two
+  * values are NEARLY equal, this method will return true.
+  */
+ public boolean equals(MyDouble passedVal) {
+   if (passedVal == null){
+      return false;
+   }
+   
+   return Math.abs(this.doubleValue - passedVal.doubleValue) < EPSILON;
+ }
 	
-	/**
-	 * Returns the absolute value of the current object.
-	 * @return a MyDouble representing the absolute value of the current object.
-	 */
-	public MyDouble abs() {
-		return new MyDouble(Math.abs(this.doubleValue));
-	}
+ /**
+  * Returns the absolute value of the current object.
+  * @return a MyDouble representing the absolute value of the current object.
+  */
+ public MyDouble abs() {
+   return new MyDouble(Math.abs(this.doubleValue));
+ }
 
-	/**
-	 * YOU MAY NOT CALL THIS METHOD EXCEPT WHILE YOU ARE IMPLEMENTING
-	 * THE toString METHOD OF THE COMPLEX NUMBER CLASS!!
-	 * 
-	 * Returns a String representation of the current object.
-	 */
-	public String toString() {
-		toStringUsed = true;
-		return new Double(this.doubleValue).toString();
-	}
+ /**
+  * YOU MAY NOT CALL THIS METHOD EXCEPT WHILE YOU ARE IMPLEMENTING
+  * THE toString METHOD OF THE COMPLEX NUMBER CLASS!!
+  * 
+  * Returns a String representation of the current object.
+  */
+ public String toString() {
+   toStringUsed = true;
+   return new Double(this.doubleValue).toString();
+  }
 }
