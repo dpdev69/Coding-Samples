514df00e535c20bc3a387006cd851668e2f32ff6
etaylor5 <etaylor5@terpmail.umd.edu>
2013-12-14 06:19:24 -0500

    Create IntegerComparator.java

---
+++ CMSC132-Project2/IntegerComparator.java
@@ -0,0 +1,9 @@
+package list;
+
+import java.util.Comparator;
+
+public class IntegerComparator implements Comparator<Integer> {
+  public int compare(Integer a, Integer b) 
+    return a.compareTo(b);
+  }
+}
