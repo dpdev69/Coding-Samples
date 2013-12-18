5ef4e05aadb079b3a9069c6bd7ef64bd0e0eaae8
Emmanuel Taylor <etaylor5@terpmail.umd.edu>
2013-12-18 01:23:47 -0500

    Sliding Puzzle (Prolog)

---
+++ CMSC330-Project6/publicRecursion2.pl
@@ -0,0 +1,20 @@
+/* swap values u,v in list b, assume all list elements are unique */
+
+list_swap_test(L, X, Y) :- findall(V, list_swap_val(L, X, Y, V), R), write(R), ml.
+list_swap_public :-
+  list_swap_test([5, 6, 7, 3], 7, 5),
+  list_swap_test([5, 6, 7, 3], 6, 5),
+  list_swap_test([5, 6, 7, 3], 3, 5),
+  list_swap_test([5, 6, 7, 3], 7, 3).
+  
+/* returns inde of value v in list x, if found &/
+
+index_test(L, X) :- findall(V, index(L, X, V), R), write(R), nl.
+index_public :-
+  index_test([5, 6, 7, 3], 5),
+  index_test([5, 6, 7, 3], 6),
+  index_test([5, 6, 7, 3], 7),
+  index_test([5, 6, 7, 3], 3),
+  index_test([7, 5, 6, 5], 5),
+  index_test([7, 5, 6, 5], 4).
+  
