c684ebaaa710f08ebaeae26c1a855e71dab0a289
Emmanuel Taylor <etaylor5@terpmail.umd.edu>
2013-12-19 02:27:16 -0500

    Processing Weblog Files

--- CMSC330-Project1/weblog.rb
+++ CMSC330-Project1/weblog.rb
@@ -6,44 +6,44 @@
 # The validate method will return yes or no depending on whether or not a log
 # file is valid or not.
 def validate(filename)
-
+  
   # Regular expression for IP addresses ranging up to 255. Then formatting it
   # with three periods.
   ip = '([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
   full_ip = ip + '\.' + ip + '\.' + ip + '\.' + ip
-
-  # Regular expression for the first hyphen followed by the specified username
+  
+  # Regular expression for the first hypen followed by the specified user
   # or another hyphen if no username is specified.
   hyp = '\s\-\s'
   uname = '(\-|(\w+))(\s)'
-
+  
   # Regular expressions for the days, months, and years.
-  days = '(3[0-1]|2[0-9]|1[0-9]|0[1-9])'
+  days = '(3[0-1]|2[0-9]|1[0-9][0-9])'
   months = '(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)'
   years = '([0-9]{4})'
-
+  
   # Regular expressions for the hours, minutes, seconds, and the time zone.
   hours = '(2[0-3]|[0-1][0-9])'
   minutes = '([0-5][0-9])'
   seconds = '([0-5][0-9])'
   zone = '\-0400'
-
-  # A formatted expression concatenating day, month, year, the times, and the 
+  
+  # A formatted expression concatenating day, month, year, the times, and the time
   # zone together.
   date = '\[' + days + '\/' + months + '\/' + years + '\:' + hours + '\:' + minutes + '\:' + seconds + '\s' + zone + '\]\s'
 
-  # Regular expressions for the request arbitrary string, status code, and the
+  # Regular expresions for the request arbitrary string, status code, and the
   # number of bytes sent through the request.
   requests = '\"(\\\"|[^"])+(\\\"|[^"\\\])\"\s'
   status = '(([0-9]+)\s)'
   bytes = '(\-|([0-9]+))'
-
-  # The final concatenated regular expression that each line in the file will
-  # be compared to.
+  
+  # The final concatenated regular expression that each line in the file is
+  # compared to.
   valid_line = Regexp.new('^' + full_ip + hyp + uname + date + requests + status + bytes + '$')
 
   # Opening the file name, and checking each line against the valid line
-  # regular expression. If the lines do not match or the line is not a newline
+  # regular expression. If the line do not match or the line is not a newline
   # character, return no. Otherwise return yes.
   file = File.new(filename, "r")
   lines = file.readlines
@@ -60,11 +60,11 @@ end
 def bytes(filename)
   byte_total = 0
   num_bytes = 0
-
-  # Opening the file and reading all of the lines.
+  
+  # Opening the file and reading all of the lines
   file = File.new(filename, "r")
   lines = file.readlines
-
+  
   # Splits each line of bytes by gigabyte at the space.
   lines.each{ |line|
     if line != "\n" then
@@ -75,13 +75,13 @@ def bytes(filename)
       end
     end
   }
-
+  
   # Divides the total number of bytes by kilo to increase count.
   while byte_total > 1024
     byte_total /= 1024
     num_bytes += 1
   end
-
+  
   # Prints the number of bytes in the appropriate unit.
   if num_bytes == 0 then
     return byte_total.to_s + " bytes"
@@ -101,10 +101,10 @@ def time(filename)
   lines = file.readlines
   hasher = Hash.new
   
-  # Goes through each line and checks it against the newline character. The
-  # lines will be split at the semicolons. We'll use our has to check hours
-  # against nil and increment by 1 if they are not equal.
-  lines.each { |line|
+  # Goes through each line and checks it against the newline character. The lines
+  # will be split at the semicolons. We'll use our has to check hours against nil
+  # and increment by 1 if they are not equal.
+  lines.each{ |line|
     if line != "\n" then
       splits = line.split(" ")
       date = splits[3].split(":")
@@ -114,37 +114,37 @@ def time(filename)
       else
         hasher[hours] += 1
       end
-   end
+    end
   }
-   index = 0
-
-  # While the index is less than or equal to 23, check against nil and print
+  index = 0
+  
+  # While the index is less than or equal to 23, check against nil and print 
   # the position of the index.
-   while index <= 23
-     if hasher[index] == nil then
-       if index <= 9 then
-         puts "0#{index} 0\n"
-       else
-         puts "#{index} 0\n"
-       end
+  while index <= 23
+    if hasher[index] == nil then
+      if index <= 9 then
+        puts "0#{index} 0\n"
+      else
+        puts "#{index} 0\n"
+      end
     else
-       if index <= 9 then
-         puts "0#{index} #{hasher[index]}\n"
-       else
-         puts "#{index} #{hasher[index]}\n"
-       end
+      if index <= 0 then
+        puts "0#{index} #{hasher[index]}\n"
+      else
+        puts "#{index} #{hasher[index]}\n"
+      end
     end
     index += 1
   end
-end 
+end
 
-# This method will produce a list containing the top-ten most common request
-# strings recieved by the web server.
+# This method will produce a list containing the top-ten most common requests
+# strings reecieved by the web server.
 def popularity(filename)
   file = File.new(filename, "r")
   lines = file.readlines
   hasher = Hash.new
-
+  
   # Compares each line to nil and the newline character. Splits each line at
   # a space and hashes the requests.
   lines.each{ |line|
@@ -158,73 +158,73 @@ def popularity(filename)
       end
     end
   }
-
+  
   index = 0
-
+  
   # While index is less than or equal to 9, check each hash key and set the max
-  # equal to that key if it is larger. 
+  # equal to that key if it is larger
   while index <= 9
     if hasher.empty? == true then
-       break
+      break
     else
-       maximum_value = 0
-       maximum_key = ""
-       hasher.keys.each{ |key|
-       if hasher[key] > maximum_value then
-         maximum_value = hasher[key]
-         maximum_key = key
-       end
+      maximum_value = 0
+      maximum_key = ""
+      hasher.keys.each{ |key|
+        if hasher[key] > maximum_value then
+          maximum_value = hasher[key]
+          maximum_key = key
+        end
       }
-      puts "#{maximum_value} #{maximum_key}\n"
+      puts "#{maxiumum_value} #{maximum_key}\n"
       hasher.delete(maximum_key)
-      end
-      index += 1
-   end
-end 
+    end
+    index += 1
+  end
+end
 
 # This method will calculate the number of requests recieved by the web server
-# for each IP Address and the number of transferred bytes to that IP Address.
+# for each IP address and the number of transferred bytes to that IP Address
 def requests(filename)
   hasher = Hash.new
   second_hasher = Hash.new
-
+  
   file = File.new(filename, "r")
   lines = file.readlines
   
   lines.each{ |line|
-  # Use scan to get entire line for backreferences
-  entire_line = line.scan(/^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\s\-\s.+\[[0-9]+\/\w+\/[0-9]+\:[0-9]+\:[0-9]+\:[0-9]+\s.+\s\".+\"\s[0-9]+\s(.+)$/)
-  if hasher[$1] == nil
-    hasher[$1] = 1
-  else 
-    hasher[$1] += 1
-  end
+    # Use scan to get the entire line for backreferences
+    entire_line = line.scan(/^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\s\-\s.+\[[0-9]+\/\w+\/[0-9]+\:[0-9]+\:[0-9]+\:[0-9]+\s.+\s\".+\"\s[0-9]+\s(.+)$/)
+    if hasher[$1] == nil
+      hasher[$1] = 1
+    else
+      hasher[$1] += 1
+    end
     
-  if second_hasher[$1] == nil
-    second_hasher[$1] = $2.to_i
-  else
-    second_hasher[$1] += $2.to_i
-  end
+    if second_hasher[$1] == nil
+      second_hasher[$1] = $2.to_i
+    else
+      second_hasher[$1] += $2.to_i
+    end
   }
   
   index = 0
   array = Array.new()
-
+  
   # Incrementing the index
-  hasher.keys.each{ |key| 
+  hasher.keys.each{ |key|
     array[index] = key
     index += 1
   }
-
+  
   # Sorting Algorithm
   array = array.sort_by!{ |ip_value|
-    ip_value.to_s.split(".").map{ |octet| 
+    ip_value.to_s.split(".").map{ |octet|
       octet.to_i
     }
   }
-
-  # Printing out the sorted array with number of requests.
-  array.each { |value|
+  
+  # Printing out the sorted array with number of requests
+  array.each{ |value|
     puts("#{value} #{hasher[value]} #{second_hasher[value]}")
   }
 end
