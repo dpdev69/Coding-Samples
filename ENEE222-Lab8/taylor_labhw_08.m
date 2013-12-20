740fe04aa2fe10be1fa70f13bf592a34bda52923
Emmanuel Taylor <etaylor5@terpmail.umd.edu>
2013-12-20 05:37:44 -0500

    Lab Assignment 8

---
+++ ENEE222-Lab8/taylor_labhw_08.m
@@ -0,0 +1,12 @@
+%% Emmanuel Taylor
+% Elements of Discrete Signal Analysis
+% Lab Assignment 08
+
+% Matrix X defined by me.
+X = [0 1 2 3;4 5 6 7];
+
+% Value M defined by me.
+M = 3;
+
+% Call to compress2 function.
+[Y,K,L,R] = compress2(X,M)
